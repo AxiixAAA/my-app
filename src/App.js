e17669f6bfb53bc6cea57114f0ebe8cc39246412
AxiixAAA <slavae22@mail.ru>
2022-04-04 11:57:17 +0500

    Teme_3

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Preloader from "./commponents/Common/Preloader/Preloader"
 import HeaderContainer from "./commponents/Header/HeaderContainer"
 import { UsersPage } from "./commponents/Users/UsersContainer"
 import { LoginPage } from "./commponents/Login/Login"
-import { Box } from "@mui/material"
+import { Box, Grid } from "@mui/material"
 
 const  DialogsContainer = React.lazy(() => import('./commponents/Dialogs/DialogsContainer'))
 const  ProfileContainer = React.lazy(() => import('./commponents/Profile/ProfileContainer'))
@@ -39,25 +39,34 @@ class App extends Component {
         return <Preloader />
         }
         return (
-            <Box className="app-wrapper"  sx={{bgcolor: 'background.default', color: 'text.primary'}}>
-
-                <Suspense fallback={<div>Загрузка...</div>}>
-
-                <HeaderContainer />
-                <Navbar />
-                    <section className="app-wrapper-content">
-                        <Switch >
-                            <Redirect exact from="/" to="/profile" />
-                            <Route path="/profile/:userId?" render={() => <ProfileContainer />}/>   
-                            <Route path="/dialogs" render={() => <DialogsContainer />}/>
-                            <Route path="/users"   render={() => <UsersPage pageTitle={"friends"} />} />
-                            <Route path="/login"   render={() => <LoginPage />} />
-                            <Route path="/chat"   render={() => <ChatPage />} />
-                            <Route path="*"        render={() =>  <div>404 NOTE FOUND</div>}/>
-                        </Switch>
-                    </section>
-                </Suspense>
-            </Box>
+            <Grid container className="app-wrapper" >
+                <Grid item xs={7}>
+                    <Grid item xs={12}>
+                        <HeaderContainer />
+                    </Grid>
+                    <Grid item xs={12} container>
+                        <Grid item xs={3}>
+                            <Navbar />  
+                        </Grid>
+                        <Grid item xs={9}>
+                            <Suspense fallback={<div>Загрузка...</div>}> 
+                                <section className="app-wrapper-content">
+                                    <Switch >
+                                        <Redirect exact from="/" to="/profile" />
+                                        <Route path="/profile/:userId?" render={() => <ProfileContainer />}/>   
+                                        <Route path="/dialogs" render={() => <DialogsContainer />}/>
+                                        <Route path="/users"   render={() => <UsersPage pageTitle={"friends"} />} />
+                                        <Route path="/login"   render={() => <LoginPage />} />
+                                        <Route path="/chat"    render={() => <ChatPage />} />
+                                        <Route path="*"        render={() =>  <div>404 NOTE FOUND</div>}/>
+                                    </Switch>
+                                </section>
+                            </Suspense>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                
+            </Grid>
         )
     }
 }
--- src/MyApp.js
+++ src/MyApp.js
@@ -1,19 +1,12 @@
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from './App';
+import { createStyles, makeStyles } from '@mui/styles';
 
 export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 export default function MyApp() {
   const [mode, setMode] = React.useState('light');
-
-// React.useEffect(() => {
-//     setMode(JSON.parse(window.localStorage.getItem('mode')));
-//   }, []);
-
-//   React.useEffect(() => {
-//     window.localStorage.setItem('mode', mode);
-//   }, [mode]);
 
   const colorMode = React.useMemo(
     () => ({
@@ -25,21 +18,69 @@ export default function MyApp() {
   );
 
  
-  const theme = React.useMemo(
+const theme = React.useMemo(
     () =>
       createTheme({
         palette: {
-          mode,
-        },
-      }),
+          	...(mode==='dark'&&{
+				background:{
+				default: '#0a0a0a',
+				paper: '#1f1f1f',
+				},
+          	}),
+			text:{
+				...(mode === 'light'
+				? {
+				  primary: '#0a0a0a',
+				  secondary: '#0a0a0a',
+				}
+				: {
+				  primary: '#dfe4ea',
+				  paper: '#ffffff',
+				}),
+			},
+			...(mode === 'light' && {
+				background: {
+					default: '#dfe4ea',
+					paper: '#ffffff'
+				}
+			})
+        }
+    }),
     [mode],
   );
 
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
+
+//  Темизация body
+const useStyles = makeStyles(() =>
+  createStyles({
+    '@global': {
+      
+      body: {
+        ...(mode==='dark'&&{
+          background:'#0a0a0a',
+      }),
+        ...(mode==='light'&&{
+        background:'#ffffff',
+      })
+     
+      }
+    }
+  })
+);
+
+const GlobalStyles = () => {
+  useStyles();
+
+  return null;
+};
+
+return (
+  <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
+  </ColorModeContext.Provider>
+);
 }
